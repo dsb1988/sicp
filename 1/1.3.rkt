c1722b206c69996a35d86635f9fd0a5034992d15
Dmitriy Bondarenko <dsb@anahoret.com>
2013-03-06 11:22:11 +0200

    sqr added to 1.3

--- 1/1.3.rkt
+++ 1/1.3.rkt
@@ -1,5 +1,6 @@
 #lang racket
+(define (sqr x) (* x x))
 (define (sotbs x y z) 
   (if (>= x y)
-      (+ (sqr x)(sqr (if (>= y z) y z)))
-      (+ (sqr y)(sqr (if (>= x z) x z)))))
+      (+ (sqr x) (sqr (if (>= y z) y z)))
+      (+ (sqr y) (sqr (if (>= x z) x z)))))
